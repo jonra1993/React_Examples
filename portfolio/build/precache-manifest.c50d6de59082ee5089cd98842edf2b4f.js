333c406c12c2cad4c3f529bcbe6657f86f96295a
Jona <jon_ra@hotmail.es>
2019-07-24 16:18:18 -0500

    build

---
+++ portfolio/build/precache-manifest.c50d6de59082ee5089cd98842edf2b4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cd4f3a1952df845eeb38489f5a894d",
+    "url": "/React_Examples/index.html"
+  },
+  {
+    "revision": "68d03c4756616b74590e",
+    "url": "/React_Examples/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "354e3b80af9df09de38e",
+    "url": "/React_Examples/static/js/2.2b1ab20e.chunk.js"
+  },
+  {
+    "revision": "68d03c4756616b74590e",
+    "url": "/React_Examples/static/js/main.8b5ee0fc.chunk.js"
+  },
+  {
+    "revision": "3c9f62d6ea8b97b25fc9",
+    "url": "/React_Examples/static/js/runtime~main.15ad6e8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_Examples/static/media/logo.5d5d9eef.svg"
+  }
+]);
