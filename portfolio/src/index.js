588bbebd77dd031982978bf8647f99ada432a942
Jona <jon_ra@hotmail.es>
2019-07-24 16:03:03 -0500

    Add top bar of material ui

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './routes/game/Game';
+import App from './routes/app/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
